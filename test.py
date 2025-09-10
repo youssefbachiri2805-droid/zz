a11192c6e19ed9195be701dd9feb70ae2fb8c3fc
yousstestyes <youssefbachiri2805@gmail.com>
2025-09-10 17:23:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
-import stanza
-stanza.download('fr')  # télécharge le modèle français
-nlp = stanza.Pipeline(lang='fr', processors='tokenize,ner')
-doc = nlp("MME IMANE BACHIRI")
-for ent in doc.ents:
-    print(ent.text, ent.type)  # e.g. "Marie Curie PERSON"
-    nom_famille = ent.text.split()[-1]
-    print("Nom de famille :", nom_famille)  # => Curie
+import pandas as pd
+import re
+
+def extraire_nom(beneficiaire: str, prenoms: set):
+    """
+    Extrait le nom (mot avant le prénom) si un prénom est trouvé dans la chaîne.
+    """
+    if pd.isna(beneficiaire):
+        return None
+
+    mots = re.findall(r"\b\w+\b", beneficiaire.upper())
+    for i, mot in enumerate(mots):
+        if mot in prenoms and i > 0:
+            return mots[i-1]  # mot juste avant le prénom
+    return None
